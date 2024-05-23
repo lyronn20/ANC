01fbf70f5c54b9b199fd1f81d907dab2cf318515
lyronn <lyronn2012@gmail.com>
2024-05-23 09:32:26 +0200

    changements

--- PHP/index.php
+++ PHP/index.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style-index.css">
     <title>ANC</title>
 </head>
 <body>
+    <h1>ANC Conciergerie</h1>
+    <div class="navbar">
+        <ul>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="connexion.php"><button><span></span>Connexion</button></a></li>
+        </ul>
+    </div>
 
 </body>
 </html>
 
 <?php
-    include 'connexionDB.php';
+    include 'connectionDB.php';
 ?>
